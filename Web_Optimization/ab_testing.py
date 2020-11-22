59c9fd5342af84903395c572f6ee808ce6006f28
kiccho1101 <youodf11khp@gmail.com>
2020-11-23 08:56:50 +0900

    add diff table & multiple ab testing

--- Web_Optimization/ab_testing.py
+++ Web_Optimization/ab_testing.py
@@ -1,28 +1,62 @@
 import seaborn as sns
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 import pymc3 as pm
+from itertools import combinations
+
+from typing import List
 
 
 def ab_testing():
-    trials_str: str = st.text_input("N_A, a_A, N_B, a_B", value="30, 5, 40, 3").replace(
-        " ", ""
-    )
-    N_A, a_A, N_B, a_B = [int(s) for s in trials_str.split(",")]
-    with pm.Model() as model:
-        with st.spinner("Running MCMC..."):
-            theta = pm.Uniform("theta", lower=0, upper=1, shape=2)
-            obs = pm.Binomial("obs", p=theta, n=[N_A, N_B], observed=[a_A, a_B])
-            trace = pm.sample(5000, chains=2, tune=1000, cores=1)
+    N_str: str = st.text_input("N", value="434, 382, 394, 88").replace(" ", "")
+    a_str: str = st.text_input("a", value="8, 17, 10, 4").replace(" ", "")
+    button = st.button("Run MCMC")
+    if button:
+        N: List[int] = [int(s) for s in N_str.split(",")]
+        a: List[int] = [int(s) for s in a_str.split(",")]
+        print(N)
+        print(a)
+        with pm.Model() as model:
+            with st.spinner("Running MCMC..."):
+                theta = pm.Uniform("theta", lower=0, upper=1, shape=len(N))
+                obs = pm.Binomial("obs", p=theta, n=N, observed=a)
+                trace = pm.sample(5000, chains=2, tune=1000, cores=1)
+            for i in range(len(N)):
+                sns.distplot(
+                    trace["theta"][:, i], label=chr(65 + i), bins=100, kde=True
+                )
+            plt.legend()
+            st.pyplot()
 
-        st.balloons()
-        sns.distplot(trace["theta"][:, 0], label="A", color="red", bins=100, kde=True)
-        sns.distplot(trace["theta"][:, 1], label="B", color="blue", bins=100, kde=True)
-        plt.legend()
-        st.pyplot()
+            summary = pm.summary(trace, hdi_prob=0.95)
+            st.table(summary)
 
-        summary = pm.summary(trace, hdi_prob=0.95)
-        st.table(summary)
-        diff = (trace["theta"][:, 1] - trace["theta"][:, 0] > 0).mean() * 100
-        st.subheader("p(A - B) > 0: {}%".format(diff if diff > 50 else 100 - diff))
+            st.subheader("Difference")
+            diff_df = pd.DataFrame()
+            for subset in combinations(list(range(len(N))), 2):
+                diff = (
+                    trace["theta"][:, subset[0]] - trace["theta"][:, subset[1]] > 0
+                ).mean() * 100
+                diff = diff if diff > 50 else 100 - diff
+                diff_df = pd.concat(
+                    [
+                        diff_df,
+                        pd.DataFrame(
+                            [
+                                {
+                                    "trials": "{} - {}".format(
+                                        chr(65 + subset[0]), chr(65 + subset[1])
+                                    ),
+                                    "diff": "{}%".format(diff),
+                                }
+                            ]
+                        ),
+                    ]
+                )
+            st.table(diff_df.sort_values("diff", ascending=False).set_index("trials"))
+
+            st.subheader("Forest Plot")
+            pm.forestplot(trace, combined=True, hdi_prob=0.95)
+            st.pyplot()
